4e0008e81bcd9386c168e00c6ef34d021231ddc4
Edgar Olvera <eols7767@gmail.com>
2021-10-13 13:14:11 -0500

    Add files via upload

---
+++ Website Files/ArtistsEssentialsWebsite/Controllers/AlbumController.cs
@@ -0,0 +1,191 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ArtistsEssentialsWebsite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtistsEssentialsWebsite.Controllers
+{
+    public class AlbumController : Controller
+    {
+
+        readonly HttpClient client = new();
+
+        // GET: Album
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<ActionResult> ShowAlbums(string genre)
+        {
+            //  Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+            //  Json File for openweathermap.ord is all lowercase
+            //  We assign case insesitivity to handle lower case names
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var response = client.GetStringAsync("http://localhost:8080/api/EssentialAlbums?genre="+genre);
+
+            List<Album> artists = new();
+            try
+            {
+                var msg = await response;
+                //Console.WriteLine(msg);
+                artists = JsonSerializer.Deserialize<List<Album>>(msg, options);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Artist");
+            }
+
+            return View(artists);
+        }
+
+        // GET: Album/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Album/Create?artistId
+        public ActionResult Create(long artistId)
+        {
+            return View();
+        }
+
+        // POST: Album/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Album @album)
+        {
+            try
+            {
+                var response = await client.PostAsJsonAsync("http://localhost:8080/api/EssentialAlbums", @album);
+                return RedirectToAction("Details", "Artist", new { id = @album.EssentialArtistId });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Album/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            //  Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+            //  Json File for openweathermap.ord is all lowercase
+            //  We assign case insesitivity to handle lower case names
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var response = client.GetStringAsync("http://localhost:8080/api/EssentialAlbums/" + id);
+
+            Album album = new();
+
+            try
+            {
+                var msg = await response;
+
+                album = JsonSerializer.Deserialize<Album>(msg, options);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(album);
+        }
+
+        // POST: Album/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, Album @album)
+        {
+            try
+            {
+                var url = "http://localhost:8080/api/EssentialAlbums/" + id;
+
+                var response = await client.PutAsJsonAsync(url, @album);
+
+                return RedirectToAction("Details", "Artist", new { id = @album.EssentialArtistId });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Album/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Album/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Album @album)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [BindProperty]
+        public long EssentialArtistId { get; set; }
+
+        [BindProperty]
+        public string Title { get; set; }
+        [BindProperty]
+        public string Genre { get; set; }
+        [BindProperty]
+        public int ReleaseYear { get; set; }
+        [BindProperty]
+        public string CreatedBy { get; set; }
+        [BindProperty]
+        public string CoverImage { get; set; }
+        [BindProperty]
+        public int? BillboardPeakUS { get; set; }
+        [BindProperty]
+        public int? WeeksOnChartUS { get; set; }
+        [BindProperty]
+        public string Certification { get; set; }
+        [BindProperty]
+        public decimal? PitchforkRating { get; set; }
+        [BindProperty]
+        public decimal? MetacriticRating { get; set; }
+        [BindProperty]
+        public decimal? RollingStoneRating { get; set; }
+        [BindProperty]
+        public decimal? RateYourMusicRating { get; set; }
+        [BindProperty]
+        public int? GrammyNominations { get; set; }
+        [BindProperty]
+        public int? GrammyWins { get; set; }
+    }
+}
---
+++ Website Files/ArtistsEssentialsWebsite/Controllers/ArtistController.cs
@@ -0,0 +1,231 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ArtistsEssentialsWebsite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace ArtistsEssentialsWebsite.Controllers
+{
+    public class ArtistController : Controller
+    {
+        readonly HttpClient client = new();
+
+        // GET: Artist
+        public ActionResult Index()
+        {
+            List<string> genres = new() { "pop", "hip hop", "soul", "rock", "electronic", "country", "metal", "punk" , "jazz" };
+            return View(genres);
+        }
+
+
+        public async Task<ActionResult> ShowArtists(string genre)
+        {
+            //  Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+            //  Json File for openweathermap.ord is all lowercase
+            //  We assign case insesitivity to handle lower case names
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var response = client.GetStringAsync("http://localhost:8080/api/EssentialArtists?genre="+genre);
+
+            List<Artist> artists = new();
+            try
+            {
+                var msg = await response;
+                //Console.WriteLine(msg);
+                artists = JsonSerializer.Deserialize<List<Artist>>(msg, options);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(artists);
+        }
+
+        // GET: Artist/Details/5
+        public async Task<ActionResult> Details(int id, string sortby = "critics")
+        {
+            //  Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+            //  Json File for openweathermap.ord is all lowercase
+            //  We assign case insesitivity to handle lower case names
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var response = client.GetStringAsync("http://localhost:8080/api/EssentialArtists/" + id);
+
+            List<Album> albums = new();
+            try
+            {
+                var msg = await response;
+
+                var artist = JsonSerializer.Deserialize<Artist>(msg, options);
+
+                ViewData["artistName"] = artist.Name;
+                ViewData["artistID"] = artist.Id;
+                ViewData["artistGenre"] = artist.Genre;
+                albums = artist.EssentialAlbums;
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            if (sortby == "critics")
+            {
+                ViewData["isChecked_co"] = "";
+                ViewData["isChecked_aw"] = "";
+                ViewData["isChecked_on"] = "";
+                ViewData["isChecked_cr"] = "checked";
+                return View(albums.OrderByDescending(a => a.PitchforkRating).ThenByDescending(a => a.RollingStoneRating));
+            }
+            else if (sortby == "online")
+            {
+                ViewData["isChecked_co"] = "";
+                ViewData["isChecked_aw"] = "";
+                ViewData["isChecked_cr"] = "";
+                ViewData["isChecked_on"] = "checked";
+                return View(albums.OrderByDescending(a => a.RateYourMusicRating).ThenByDescending(a => a. MetacriticRating));
+            }
+            else if (sortby == "commercial")
+            {
+                ViewData["isChecked_aw"] = "";
+                ViewData["isChecked_cr"] = "";
+                ViewData["isChecked_on"] = "";
+                ViewData["isChecked_co"] = "checked";
+                return View(albums.OrderBy(a => a.BillboardPeakUS).ThenByDescending(a => a.WeeksOnChartUS));
+            }
+            else if (sortby == "awards")
+            {
+                ViewData["isChecked_cr"] = "";
+                ViewData["isChecked_co"] = "";
+                ViewData["isChecked_on"] = "";
+                ViewData["isChecked_aw"] = "checked";
+                return View(albums.OrderByDescending(a => a.GrammyWins).ThenByDescending(a => a.GrammyNominations));
+            }
+
+            return View(albums);
+        }
+
+        // GET: Artist/Create
+        public ActionResult Create(string genre)
+        {
+            return View();
+        }
+
+        // POST: Artist/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("Name,Genre,Image")] Artist @artist)
+        {
+            try
+            {
+                var response = await client.PostAsJsonAsync("http://localhost:8080/api/EssentialArtists", @artist);
+
+                
+                //Console.WriteLine(response.EnsureSuccessStatusCode());
+
+                return RedirectToAction("ShowArtists", new { @artist.Genre });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Artist/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            //  Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+            //  Json File for openweathermap.ord is all lowercase
+            //  We assign case insesitivity to handle lower case names
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var response = client.GetStringAsync("http://localhost:8080/api/EssentialArtists/" + id);
+
+            Artist artist = new();
+
+            try
+            {
+                var msg = await response;
+
+                artist = JsonSerializer.Deserialize<Artist>(msg, options);      
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(artist);
+        }
+
+        // PuT: Artist/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, [Bind("Id,Name,Genre,Image")] Artist @artist)
+        {
+            try
+            {
+                var url = "http://localhost:8080/api/EssentialArtists/" + id;
+
+                var response = await client.PutAsJsonAsync(url, @artist);
+
+                return RedirectToAction("ShowArtists", new { @artist.Genre });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Artist/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Artist/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
