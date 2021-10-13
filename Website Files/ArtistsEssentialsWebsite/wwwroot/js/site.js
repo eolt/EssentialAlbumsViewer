dd7ecdecfe9d21683cd11e1a5121a5362ab7628b
Edgar Olvera <eols7767@gmail.com>
2021-10-13 12:42:25 -0500

    Add files via upload

---
+++ Website Files/ArtistsEssentialsWebsite/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function sortByCritics(id) {
+    window.location.href = "/Artist/Details/"+String(id);
+}
+
+function sortByCommercial(id) {
+    window.location.href = "/Artist/Details/" + String(id) +"?sortby=commercial";
+}
+
+function sortByAwards(id) {
+    window.location.href = "/Artist/Details/" + String(id) +"?sortby=awards";
+}
+
+function sortByOnline(id) {
+    window.location.href = "/Artist/Details/" + String(id) + "?sortby=online";
+}
+
+
+
